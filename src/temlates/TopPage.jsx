fe48153dba56075576416c4cfa1a506b8c20cb1f
koumori-07 <2kw-2n-w2o@ezweb.ne.jp>
2020-12-09 23:03:57 +0900

    Link

--- src/temlates/TopPage.jsx
+++ src/temlates/TopPage.jsx
@@ -3,15 +3,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { getArticles } from '../reducks/article/selector';
 import { ArticleCard } from '../components/Article';
 import { fetchArticle } from '../reducks/article/operation';
+import { Button } from '@material-ui/core';
+import { push } from 'connected-react-router';
 
 
 const TopPage = () => {
     const selector = useSelector((state) => state);
     const articles = getArticles(selector)
+    const dispatch = useDispatch();
     console.log(articles)
     return( 
 
         <section>
+           <Button onClick={() => dispatch(push('/new'))}>新規投稿</Button>
             {articles.length > 0 &&(
                 articles.map(article=>(
                     <ArticleCard key={article.id}article={article}/>
